54f3d5765d7463006b4553a3bab75e4096de3ca0
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2022-12-25 22:38:18 -0600

    img shift

--- flip.py
+++ flip.py
@@ -9,6 +9,8 @@ def main(input_file):
     
     upsidedown = ''
     backwards = ''
+    int_entered = False
+    
 
     while (upsidedown != 'y' and upsidedown != 'n' ):
         print('upsidedown? (y/n)')
@@ -18,6 +20,18 @@ def main(input_file):
         print('backwards? (y/n)')
         backwards = input()
 
+    while (not int_entered):
+        print('amount?')
+        shift = input()
+        try:
+            shift = int(shift)
+            int_entered = True
+        except:
+            print("enter an amount to shift down")
+        
+    print(upsidedown, backwards)
+        
+
 
 
     image = PIL.Image.open(input_file)
@@ -26,12 +40,29 @@ def main(input_file):
     if(upsidedown=='y'):
         array = np.flip(array)
    
-    if(backwards=='n'):
+    if(backwards=='y'):
         for i in range(array.shape[0]):
             array[i] = np.flip(array[i])
 
+
+    if(shift != 0):
+        new_array = np.ndarray(array.shape)
+        if(shift<0):
+            for i in range(shift,array.shape[0]):
+                new_array[i] = array[i+shift]
+        else:
+            for i in range(array.shape[0]-shift):
+                new_array[i] = array[i+shift]
+        array = new_array
+            
+
+
+
+
     image = im.fromarray(array) 
     image = image.convert("L")
+    input_file = input_file.split('.')
+    input_file = input_file[0]
     image.save(input_file+"_flipped.png")
 
 
